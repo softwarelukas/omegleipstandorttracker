45456aea77e6c038e59f4ad5a4395279b12792bc
softwarelukas <78620381+softwarelukas@users.noreply.github.com>
2021-02-05 21:39:06 +0100

    .
    .

---
+++ tool.js
@@ -0,0 +1,41 @@
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+const getLocation = async (ip) => {
+  let url = `https://get.geojs.io/v1/ip/geo/${ip}.json`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          Land: ${json.country}
+          Stadt: ${json.city}
+          Region: ${json.region}
+          IP: ${json.ip}
+          Anbieter: ${json.organization_name}
+          Genauereinfos: ${json.organization}
+          ---------------------
+          `;
+
+      console.log(output);
+      
+    })
+  );
+};
